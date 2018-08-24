b320e0ca0c38df0851798ae4ae4784f335dd2f4d
Scott Roberson <scott.roberson@theprimacy.com>
2018-08-24 13:08:00 -0400

    adding skipnav js implementation

---
+++ skipnav.js
@@ -0,0 +1,15 @@
+// Vanilla JS version
+// link looks like:   <a href="#main" id="skipnav">skipnav</a>
+document.querySelector("#skipnav").addEventListener("click", function(event) {
+    const element = event.currentTarget;
+    const skipToID = "#" + element.getAttribute('href').split('#')[1];
+    let target = document.querySelector(skipToID);
+    let removeTabIndex = (event) => {
+        event.target.setAttribute("tabindex", "");
+    };
+
+    target.setAttribute("tabindex", -1);
+    target.focus();
+    target.addEventListener('blur', removeTabIndex);
+    target.addEventListener('focusout', removeTabIndex);
+});
