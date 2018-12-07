75b18c2cb6e53705682b125eb20edf0fef2edd34
Scott Roberson <scott.roberson@theprimacy.com>
2018-12-07 14:12:25 -0500

    adding eslint to ensure proper formatting; moving error trap

--- skipnav.js
+++ skipnav.js
@@ -1,19 +1,20 @@
 // Vanilla JS version
 // link looks like:   <a href="#main" id="skipnav">skipnav</a>
-document.querySelector("#skipnav").addEventListener("click", function(event) {
+document.querySelector("#skipnav").addEventListener("click", function (event) {
     const element = event.currentTarget;
-    const skipToID = "#" + element.getAttribute('href').split('#')[1];
+    const skipToID = "#" + element.getAttribute("href").split("#")[1];
     let target = document.querySelector(skipToID);
-    let removeTabIndex = (event) => {
-        event.target.setAttribute("tabindex", "");
-    };
 
     if (target.length === 0) {
         console.log("target not found [" + skipToID + "]");
     }
 
+    let removeTabIndex = (event) => {
+        event.target.setAttribute("tabindex", "");
+    };
+
     target.setAttribute("tabindex", -1);
     target.focus();
-    target.addEventListener('blur', removeTabIndex);
-    target.addEventListener('focusout', removeTabIndex);
+    target.addEventListener("blur", removeTabIndex);
+    target.addEventListener("focusout", removeTabIndex);
 });
